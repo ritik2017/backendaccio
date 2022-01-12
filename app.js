5459d8f88f2e7ddf331938cfb2f88f5e02fe251b
Ritik Kumar <kumarritik@microsoft.com>
2022-01-12 22:50:45 +0530

    Crud Ops

--- app.js
+++ app.js
@@ -84,16 +84,142 @@ app.post('/submit_form', async (req, res) => {
 
     if(email)
         formData.email = email;
+
+    try {
     
-    let formDb = await formData.save();
+        let formDb = await formData.save();
 
-    console.log(formDb);
+        console.log(formDb);
 
-    res.send({
-        status: 200,
-        message: "Form Submmitted Successfully",
-        data: formDb
-    });
+        res.send({
+            status: 200,
+            message: "Form Submmitted Successfully",
+            data: formDb
+        });
+    }
+    catch(err) {
+        res.send({
+            status: 400,
+            message: "Database error",
+            error: err
+        })
+    }
+})
+
+app.get('/read_forms', async (req, res) => {
+
+    // FormModel.find().then((data) => {
+    //     res.send({
+    //         status: 200,
+    //         message: "Read data successfully",
+    //         data: data
+    //     })
+    // }).catch(err => {
+    //     res.send({
+    //         status: 400,
+    //         message: "Database error",
+    //         error: err
+    //     })
+    // });    
+
+    try {
+        const formData = await FormModel.find();
+
+        res.send({
+            status: 200,
+            message: "Read data successfully",
+            data: formData
+        })
+    }
+    catch(err) {
+        res.send({
+            status: 400,
+            message: "Database error. Please try again",
+            error: err
+        })
+    }
+})
+
+app.post('/search_forms', async (req, res) => {
+
+    let searchBody = req.body; 
+
+    if(Object.keys(searchBody).length == 0) {
+        return res.send({
+            status: 401,
+            message: "Missing parameters"
+        })
+    }
+    
+    try {
+
+        let formData = await FormModel.find(searchBody);
+
+        return res.send({
+            status: 200,
+            message: "Search Successful",
+            data: formData
+        })
+
+    }
+    catch(err) {
+        return res.send({
+            status: 400,
+            message: "Database error. Please try again",
+            error: err
+        })
+    }
+})
+
+app.post('/update_form', async (req, res) => {
+
+    let id = req.body.id;
+    let newData = req.body.newData;
+
+    try {
+
+        let oldData = await FormModel.findOneAndUpdate({_id: id}, newData);
+
+        res.send({
+            status: 200,
+            message: "Updated data successfully",
+            data: oldData
+        })
+    }
+    catch(err) {
+        res.send({
+            status: 400,
+            message: "Database Error",
+            error: err
+        })
+    }
+
+})
+
+app.post('/update_forms', async (req, res) => {
+
+    let grade = req.body.grade;
+    let newData = req.body.newData;
+
+    try {
+
+        let oldData = await FormModel.updateMany({grade: grade}, {$set: newData});
+
+        res.send({
+            status: 200,
+            message: "Updated data successfully",
+            data: oldData
+        })
+    }
+    catch(err) {
+        console.log(err);
+        res.send({
+            status: 400,
+            message: "Database Error",
+            error: err
+        })
+    }
+
 })
 
 app.get('/myapi/rohan', (req, res) => {
@@ -109,8 +235,6 @@ app.get('/myapi/:id/:name', (req, res) => {
     console.log(req.query);
     res.send("Myapi Id");
 })
-
-
 
 app.listen(4000, () => {
     console.log('Listening on Port 4000');
@@ -149,3 +273,7 @@ app.listen(4000, () => {
 // How to get data in api
 // GET - query(?), params(/:)
 // POST - query(?), params(/:), body({})
+
+// fuser -k 4000/tcp - LINUX
+// netstat -ano | findstr :4000, TASKKILL /PID process_number /F - Windows
+//  ps -ef | grep 4000, kill -9 process_number - MAC
